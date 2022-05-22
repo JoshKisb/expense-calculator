968355d9ee111d2053ca49d7850d5bdc088ce891
Josh Kisb <joshuakisb@gmail.com>
2022-05-22 13:00:39 +0300

    failing test single expense

--- backend/tests/ExpenseCategoryCalcTest.php
+++ backend/tests/ExpenseCategoryCalcTest.php
@@ -3,18 +3,38 @@
 use App\ExpenseCategoryCalc;
 use PHPUnit\Framework\TestCase;
 
-class ExpenseCategoryCalcTest extends TestCase {
+class ExpenseCategoryCalcTest extends TestCase
+{
+
+   private $calc;
+
+   protected function setUp(): void
+   {
+      $this->calc = new ExpenseCategoryCalc();
+   }
+
+   protected function tearDown(): void
+   {
+      $this->calc = null;
+   }
 
    public function testEmptyInputReturnsEmptyArray()
    {
-      $calc = new ExpenseCategoryCalc();
-      $calc->addExpenses([]);
-
-      $this->assertSame($calc->getCategories(), []);
+      $this->calc->addExpenses([]);
+      $this->assertSame($this->calc->getCategories(), []);
    }
 
    public function testSingleExpenseReturnsSingleCategory()
    {
-      $this->assertSame(1, 1);
+      $expense = [
+         "category" => "Food",
+         "price" => 100,
+         "amount" => 2,
+      ];
+      $this->calc->addExpenses([$expense]);
+      $this->assertSame(
+         $this->calc->getCategories(),
+         ["Food" => 200]
+      );
    }
 }
