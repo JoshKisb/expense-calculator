fbae3ccd7d0217d4a23d11b12cf8acc970a57c92
Josh Kisb <joshuakisb@gmail.com>
2022-05-22 20:48:54 +0300

    style changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
 	return (
-		<footer>
+		<footer className="mt-5">
 			<div className="container">
 				<p className="text-center">Developed by Josh kisb &#x1F60A;</p>
 			</div>
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,38 +1,41 @@
 import { useRef } from "react";
 
 interface FormProps {
-   onSubmit: (values: FormData) => void;
+	onSubmit: (values: FormData) => void;
 }
 
-const Form: React.FC<FormProps> = ({onSubmit}) => {
+const Form: React.FC<FormProps> = ({ onSubmit }) => {
+	const formRef = useRef<HTMLFormElement>(null);
 
-   const formRef = useRef<HTMLFormElement>(null);
-
-   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const form = formRef.current;
-      if (!form) return;
-      const formData = new FormData(form);
-      onSubmit(formData);
-      form.reset();
-   }
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const form = formRef.current;
+		if (!form) return;
+		const formData = new FormData(form);
+		onSubmit(formData);
+		form.reset();
+	};
 	return (
-		<form className="form" ref={formRef} onSubmit={handleSubmit}>
-			<div>
-				<label htmlFor="csv" className="form-label">
-					Large file input example
-				</label>
-				<input
-               className="form-control form-control-lg"
-               name="csv"
-					id="csv"
-					type="file"
-				/>
-			</div>
-         <div className="mt-3 d-flex justify-content-end">
-            <button type="submit" className="btn btn-primary">Upload</button>
-         </div>
-		</form>
+		<div className="csv-form">
+			<form className="form" ref={formRef} onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="csv" className="form-label">
+						Select CSV File with the expenses data
+					</label>
+					<input
+						className="form-control form-control-lg"
+						name="csv"
+						id="csv"
+						type="file"
+					/>
+				</div>
+				<div className="mt-3 d-flex justify-content-end">
+					<button type="submit" className="btn btn-primary">
+						Upload
+					</button>
+				</div>
+			</form>
+		</div>
 	);
 };
 
